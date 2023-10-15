16ee4b859adaa521c6a101747019fc9746dc897e
Frans Åsheim <frans.asheim@student.nhh.no>
2023-10-15 17:00:06 +0200

    Created function in data_transformation.r for 4a

--- functions/data_transformations.R
+++ functions/data_transformations.R
@@ -10,3 +10,13 @@ transform_metadata_to_df <- function(metadata) {
     mutate(lat = map_dbl(location, "latLon.lat"), lon = map_dbl(location, "latLon.lon")) %>%
     select(-location)
 }
+
+
+#Function to return date variable with given offset
+to_iso8601 <<- function(dateVariable, offset) {
+  dateWithOffset <- dateVariable + days(offset)
+  dateWithOffsetISO <- iso8601(dateWithOffset)
+  dateZ <- paste(dateWithOffsetISO,"Z", sep ="")
+  return(dateZ)
+}
+to_iso8601(as_datetime("2016-09-01 10:11:12"),-4)
