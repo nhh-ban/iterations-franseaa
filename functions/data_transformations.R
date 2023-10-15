f6fd009e277c81fb6a4abdcb3d45322eda3856ff
Frans Åsheim <frans.asheim@student.nhh.no>
2023-10-15 16:20:34 +0200

    Solved for Problem 3

--- functions/data_transformations.R
+++ functions/data_transformations.R
@@ -5,7 +5,7 @@ transform_metadata_to_df <- function(metadata) {
     map(as_tibble) %>%
     list_rbind() %>% 
     mutate(latestData = map_chr(latestData, 1, .default = "")) %>% 
-    mutate(latestData = as.Date(latestData, tz = "Europe/Berlin")) %>%
+    mutate(latestData = as_datetime(latestData, tz = "UTC")) %>%
     mutate(location = map(location, unlist)) %>% 
     mutate(lat = map_dbl(location, "latLon.lat"), lon = map_dbl(location, "latLon.lon")) %>%
     select(-location)
